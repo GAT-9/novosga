aa2d5fa7da9a45a65c58b46b947f91f489b68b6f
Gabriel <felipegabrielsr03@gmail.com>
2026-05-23 21:42:26 -0300

    Renomeando algumas funções e alterando imagens(iniciando)

--- public/js/script.js
+++ public/js/script.js
@@ -237,7 +237,7 @@ const App = {
         },
         show: function(title, content) {
             if (this.allowed()) {
-                new Notification(title, { body: content, icon: App.baseUrl + '/images/favicon.png' });
+                new Notification(title, { body: content, icon: App.baseUrl + '/images/novosga-login.png' });
             } else {
                 this.request();
             }
